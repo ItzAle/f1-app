a2027de0d94f770ba09e8e423730a08642431b35
ale <alexcs9230@gmail.com>
2024-07-11 12:38:00 +0200

    test fix

--- src/apiServices/circuitService.js
+++ src/apiServices/circuitService.js
@@ -3,18 +3,24 @@ import axios from "axios";
 const API_URL = "https://f1connectapi.vercel.app/api/2024/circuits";
 
 export const getCircuits = async () => {
-  const response = await fetch("/api/circuits");
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  const responseText = await response.text();
-  console.log("Raw response:", responseText);
   try {
-    const data = JSON.parse(responseText);
-    console.log("Data received from API:", data);
-    return data;
-  } catch (e) {
-    console.error("Error parsing JSON:", e);
-    throw new Error("Failed to parse JSON response");
+    const response = await fetch("/api/circuits");
+    if (!response.ok) {
+      console.error(`HTTP error! status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const responseText = await response.text();
+    console.log("Raw response:", responseText);
+    try {
+      const data = JSON.parse(responseText);
+      console.log("Data received from API:", data);
+      return data;
+    } catch (e) {
+      console.error("Error parsing JSON:", e, "Response text:", responseText);
+      throw new Error("Failed to parse JSON response");
+    }
+  } catch (error) {
+    console.error("Error fetching data from /api/circuits:", error);
+    throw error;
   }
 };
--- src/apiServices/circuits.js
+++ src/apiServices/circuits.js
@@ -4,9 +4,7 @@ import axios from "axios";
 export default async function handler(req, res) {
   try {
     console.log("Fetching data from f1connectapi...");
-    const response = await axios.get(
-      "https://f1connectapi.vercel.app/api/2024/circuits"
-    );
+    const response = await axios.get(API_URL);
     console.log("Data fetched from f1connectapi:", response.data);
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -16,7 +14,12 @@ export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.status(200).json(response.data);
   } catch (error) {
-    console.error("Error fetching data from f1connectapi:", error);
-    res.status(500).json({ error: error.message });
+    console.error(
+      "Error fetching data from f1connectapi:",
+      error.response ? error.response.data : error.message
+    );
+    res
+      .status(500)
+      .json({ error: error.response ? error.response.data : error.message });
   }
 }
